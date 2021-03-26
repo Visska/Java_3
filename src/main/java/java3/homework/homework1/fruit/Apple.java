79b8cdf587a568a4304735a5a4a3793d12696709
Visska <strannica.ev@gmail.com>
2021-03-26 19:40:41 +0300

    clear

--- src/main/java/java3/homework/homework1/fruit/Apple.java
+++
@@ -1,8 +0,0 @@
-package java3.homework.homework1.fruit;
-
-public class Apple extends Fruit {
-
-    public Apple(float weight) {
-        super(weight);
-    }
-}
--- src/main/java/java3/homework/homework1/fruit/Orange.java
+++
@@ -1,8 +0,0 @@
-package java3.homework.homework1.fruit;
-
-public class Orange extends Fruit {
-
-    public Orange(float weight) {
-        super(weight);
-    }
-}
